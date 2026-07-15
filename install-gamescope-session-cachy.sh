22d1340bdd6863bd38c01a68c83cbea97b9934b7
stinga11 <stinga11@hotmail.com>
2026-07-15 16:08:12 -0400

    Comment out galileo-mura-setup.service installation
    Comment out the installation of galileo-mura-setup.service.

--- install-gamescope-session-cachy.sh
+++ install-gamescope-session-cachy.sh
@@ -17,7 +17,7 @@ sudo install -D -m 644 $HOME/Downloads/gamescope-session-cachyos/usr/lib/steamos
 
 
 sudo install -D -m 644 $HOME/Downloads/gamescope-session-cachyos/usr/lib/systemd/user/cachyos-gamescope-autologin.service /usr/lib/systemd/user/cachyos-gamescope-autologin.service
-sudo install -D -m 644 $HOME/Downloads/gamescope-session-cachyos/usr/lib/systemd/user/galileo-mura-setup.service /usr/lib/systemd/user/galileo-mura-setup.service
+#sudo install -D -m 644 $HOME/Downloads/gamescope-session-cachyos/usr/lib/systemd/user/galileo-mura-setup.service /usr/lib/systemd/user/galileo-mura-setup.service
 sudo install -D -m 644 $HOME/Downloads/gamescope-session-cachyos/usr/lib/systemd/user/gamescope-mangoapp.service /usr/lib/systemd/user/gamescope-mangoapp.service
 sudo install -D -m 644 $HOME/Downloads/gamescope-session-cachyos/usr/lib/systemd/user/gamescope-session.service /usr/lib/systemd/user/gamescope-session.service
 sudo install -D -m 644 $HOME/Downloads/gamescope-session-cachyos/usr/lib/systemd/user/gamescope-session.target /usr/lib/systemd/user/gamescope-session.target
